ed2d4ae523e54aa025f912957c62e597a426cb38
aShini5049 <ashinihewagamage21@gmail.com>
2025-11-17 10:51:56 +0530

    Add local storage persistence and filter options
    Tasks are now saved in localStorage, so data persists after page reload.
    
    Added filter buttons: All, Completed, and Pending to view tasks based on status.

--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,10 +1,24 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TaskItem from "./TaskItem";
 
 export default function ToDoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [filter, setFilter] = useState("all");
+
+  // load tasks from localStorage when app starts
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (savedTasks) {
+      setTasks(savedTasks);
+    }
+  }, []);
+
+  // save tasks to localStorage whenever tasks change
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function handleInputChange(e) {
     setNewTask(e.target.value);
@@ -22,6 +36,7 @@ export default function ToDoList() {
     }
   }
 
+  //select the toggle button when task is completed
   function toggleTask(id) {
     setTasks(
       tasks.map((task) =>
@@ -30,22 +45,31 @@ export default function ToDoList() {
     );
   }
 
+  //delete the added task
   function deleteTask(id) {
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
+  //apply filters - all, completed and pending
+  const filterTasks = tasks.filter((task) => {
+    if (filter === "completed") return task.completed;
+    if (filter === "pending") return !task.completed;
+    return true; //all
+  });
+
   return (
     <>
-      <div className="bg-white p-8 rounded-[10px] shadow-md w-[400px] text-center mt-[50px]">
-        <h1 className="text-2xl font-bold mb-4">📝 To-Do List App</h1>
+      <div className="bg-white p-8 rounded-[10px] shadow-md w-[400px] text-center mt-[70px]">
+        <h1 className="text-2xl font-bold mb-[50px]">📝 To-Do List App</h1>
 
+        {/*Input section + add button */}
         <div className="flex gap-[20px] mb-4">
           <input
             type="text"
             placeholder="Enter a new task..."
             value={newTask}
             onChange={handleInputChange}
-            className="flex-grow border border-gray-300 rounded-[5px] px-3 py-2 "
+            className="flex-grow border border-gray-300 rounded-[5px] px-[8px] py-2 placeholder "
           />
           <button
             onClick={addTask}
@@ -55,9 +79,33 @@ export default function ToDoList() {
           </button>
         </div>
 
-        {/* ✅ Task list */}
+        {/* Filter buttons */}
+        <div className="flex gap-[130px] justify-center mt-[50px]">
+          <button
+            onClick={() => setFilter("all")}
+            className="cursor-pointer py-[5px] text-[20px] font-semibold rounded-[10px] border-[1px] bg-[#87CEFA] text-[#00008B] transition-colors duration-300 hover:bg-[#00BFFF]"
+          >
+            All
+          </button>
+
+          <button
+            onClick={() => setFilter("completed")}
+            className=" cursor-pointer py-[5px] text-[20px] font-semibold rounded-[10px] border-[1px]  bg-[#98FB98] text-[#006400] transition-colors duration-300 hover:bg-[#90EE90]"
+          >
+            Completed
+          </button>
+
+          <button
+            onClick={() => setFilter("pending")}
+            className="cursor-pointer py-[5px] text-[20px] font-semibold rounded-[10px] border-[1px]  bg-[#FFFFC5] text-[#8B8000] transition-colors duration-300 hover:bg-[#fffd8d]"
+          >
+            Pending
+          </button>
+        </div>
+
+        {/* Task List */}
         <ol className="text-left list-decimal list-inside pl-6 mt-[50px]">
-          {tasks.map((task) => (
+          {filterTasks.map((task) => (
             <TaskItem
               key={task.id}
               task={task}
